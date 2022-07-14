2a71c31b4637af67cc7f275c9c36a4f28aaa9703
Akari <101001006+Akari0001@users.noreply.github.com>
2022-07-14 10:15:06 -0500

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  //bot token
+  token: "OTkwNzcxMTQyMjI0NjY2NjQ2.GhvGGI.loMBbnK3mXw_nG2zGrN8HSBb5yUaNq2cZoVuZ0",
+  //bot prefix
+  prefix: ">",
+
+  //devs String[]
+  devs: [941450171265474661],
+
+  /* Embeds */
+  Embed: {
+    Color: "RANDOM",
+    Error: "",
+    Succes: "<:success:989697343181979699>",
+    Denied: "<:denied:989697376669282325>",
+    Stuck: "",
+    Arrow: ">",
+    Dot: "",
+    footer: "",
+  },
+
+  /* Leaderboard */
+  Leaderboard: {
+    interval: 40000, //5 minutes = 300,000 | default is 5 mins
+  },
+
+  //mongodb url
+  mongoDB: "mongodb+srv://izzie:izzie@dandb.w50zysz.mongodb.net/?retryWrites=true&w=majority",
+
+  //repo url
+  src: "",
+};
